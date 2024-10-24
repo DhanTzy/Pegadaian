06816acbdcb097533d80bfb8bca7bbaa5bbf01bc
rayyproject <rayyproject@gmail.com>
2024-10-24 09:43:31 +0700

    txt

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -224,7 +224,6 @@
             $('#myTable').DataTable(); // Ganti '#myTable' dengan ID tabel Anda
         });
     </script>
-=======
     <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
 </body>
 </html>
