2d924eac9c51b9f253ffa4beeaeec37b38817785
rayyproject <rayyproject@gmail.com>
2025-01-30 10:51:15 +0700

    update index cetak

--- resources/views/cetak/index.blade.php
+++ resources/views/cetak/index.blade.php
@@ -1,46 +1,167 @@
 <!DOCTYPE html>
-<html lang="en">
-
+<html lang="id">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Cetak Data Transaksi</title>
+    <title>Form Pendaftaran</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            font-size: 12px
+        }
+        .container {
+            width: 80%;
+            margin: auto;
+        }
+        h2 {
+            text-align: center;
+        }
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            margin-bottom: 20px;
+        }
+        th, td {
+            border: 1px solid black;
+            padding: 20px;
+            text-align: left;
+        }
+        .header {
+            text-align: center;
+            font-size: 20px;
+            font-weight: bold;
+        }
+        .section-title {
+            font-weight: bold;
+            background-color: #ddd;
+        }
+    </style>
 </head>
-
 <body>
-    <h1 style="text-align: center;">Cetak Data Transaksi</h1>
-    <table border="1" cellpadding="5" cellspacing="0"
-        style="width: 100%; border-collapse: collapse; text-align: center;">
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Nama Debitur</th>
-                <th>Pengajuan Pinjaman</th>
-                <th>Jangka Waktu</th>
-                <th>Putusan Pinjaman</th>
-                <th>Bunga</th>
-                <th>Bunga Perbulan</th>
-                <th>Pelunasan</th>
-                <th>Biaya Administrasi</th>
-            </tr>
-        </thead>
-        <tbody>
-            @for ($i = 0; $i < 20; $i++)
-                <tr>
-                    <td>1</td>
-                    <td>Debitur 1</td>
-                    <td>Rp 10.000.000</td>
-                    <td>1 Bulan</td>
-                    <td>Rp 10.000.000</td>
-                    <td>Rp 10.000.000</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>Rp 10.000.000</td>
-                </tr>
-            @endfor
-        </tbody>
-    </table>
-</body>
+    <div class="container">
+        <div class="header">Gadai Kita</div>
+        <h2>Form Pendaftaran</h2>
+        <p><strong>Tanggal:</strong> 02/01/2025 &nbsp;&nbsp;&nbsp; <strong>No. Pendaftaran:</strong> 001/Pend-01/2025</p>
+        <p><strong>Pangkal:</strong> 012</p>
 
+        <table>
+            <tr><th colspan="2" class="section-title">Pendaftaran</th></tr>
+            <tr>
+                <td>
+                    Nama Debitur
+                    <br>
+                    NIK
+                    <br>
+                    Alamat
+                    <br>
+                    Kecamatan
+                    <br>
+                    Kabupaten
+                    <br>
+                    Provinsi
+                    <br>
+                    Tempat Lahir
+                    <br>
+                    Tanggal Lahir
+                    <br>
+                    Nomor HP
+                    <br>
+                    Pengajuan Pinjaman
+                    <br>
+                    Jangka Waktu
+                    <br>
+                    Jenis Jaminan
+                </td>
+                <td>
+                    Zubaidah
+                    <br>
+                    3509124107760009
+                    <br>
+                    Dsn. Krajan Kidul RT/RW 01/18 Sumberrejo
+                    <br>
+                    Ambulu
+                    <br>
+                    Jember
+                    <br>
+                    Jawa Timur
+                    <br>
+                    Jember
+                    <br>
+                    9 Januari 1976
+                    <br>
+                    081249555757
+                    <br>
+                    1,500,000
+                    <br>
+                    1
+                    <br>
+                    Sepeda Listrik Uwinfly
+                    <br>
+                </td>
+            </tr>
+            <tr><td colspan="2">TTD customer service :</td></tr>
+            <tr><td colspan="2">TTD debitur :</td></tr>
+            <tr><th colspan="2" class="section-title">Appraisal</th></tr>
+            <tr>
+                <td>
+                Jenis Jaminan
+                <br>
+                Nilai Pasar Wajar
+                <br>
+                Nilai Likuiditas
+                </td>
+                <td>
+                Sepeda Listrik Uwinfly
+                <br>
+                2,100,000
+                <br>
+                1,470,000
+            </td>
+        </tr>
+        <tr><td colspan="2">Tanda tangan appraisal :</td></tr>
+            <tr><th colspan="2" class="section-title">Putusan</th></tr>
+            <tr>
+                <td>
+                    Jenis Jaminan
+                    <br>
+                    Nilai Pasar Wajar
+                    <br>
+                    Nilai Likuiditas
+                    <br>
+                    Putusan Pinjaman
+                    <br>
+                    Jangka Waktu
+                    <br>
+                    Bunga %
+                    <br>
+                    Bunga Perbulan
+                    <br>
+                    Pelunasan
+                    <br>
+                    Biaya Administrasi
+                </td>
+                <td>
+                    Sepeda Listrik Uwinfly
+                    <br>
+                    2,100,000
+                    <br>
+                    1,470,000
+                    <br>
+                    1,400,000
+                    <br>
+                    1
+                    <br>
+                    8%
+                    <br>
+                    112,000
+                    <br>
+                    1,512,000
+                    <br>
+                    20,000
+                </td>
+            </tr>
+            <tr><td colspan="2">Tanda tangan pemutus :</td></tr>
+        </table>
+    </div>
+</body>
 </html>
